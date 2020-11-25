3e18efc347c60bf61171e2012126fb79d527acb6
TheOnlyTails <shacharzidon@gmail.com>
2020-11-25 19:40:30 +0200

    Fixed some problems with lang file generation (Note: not done yet)

--- src/main/kotlin/com/theonlytails/rubymod/datagen/LangGenerator.kt
+++ src/main/kotlin/com/theonlytails/rubymod/datagen/LangGenerator.kt
@@ -1,5 +1,6 @@
 package com.theonlytails.rubymod.datagen
 
+import com.theonlytails.rubymod.RubyMod
 import com.theonlytails.rubymod.RubyMod.id
 import com.theonlytails.rubymod.datagen.DataGeneratorRunner.RESOURCE_PACK
 import com.theonlytails.rubymod.registries.*
@@ -10,46 +11,47 @@ import net.minecraft.util.Identifier
 object LangGenerator {
 	fun generate() {
 		// General items
-		addLang("ruby", lang().item(ItemRegistry.RUBY, "Ruby"))
-		addLang("poisoned_apple", lang().item(ItemRegistry.POISONED_APPLE, "Poisoned Apple"))
-		addLang("ruby_sheep_spawn_egg", lang().item(ItemRegistry.RUBY_SHEEP_SPAWN_EGG, "Ruby Sheep Spawn Egg"))
+		addLang(lang()
+			.item(ItemRegistry.RUBY, "Ruby")
+			.item(ItemRegistry.POISONED_APPLE, "Poisoned Apple")
+			.item(ItemRegistry.RUBY_SHEEP_SPAWN_EGG, "Ruby Sheep Spawn Egg")
 
-		// Armor
-		addLang("ruby_helmet", lang().item(ItemRegistry.RUBY_HELMET, "Ruby Helmet"))
-		addLang("ruby_chestplate", lang().item(ItemRegistry.RUBY_CHESTPLATE, "Ruby Chestplate"))
-		addLang("ruby_leggings", lang().item(ItemRegistry.RUBY_LEGGINGS, "Ruby Leggings"))
-		addLang("ruby_boots", lang().item(ItemRegistry.RUBY_BOOTS, "Ruby Boots"))
+			// Armor
+			.item(ItemRegistry.RUBY_HELMET, "Ruby Helmet")
+			.item(ItemRegistry.RUBY_CHESTPLATE, "Ruby Chestplate")
+			.item(ItemRegistry.RUBY_LEGGINGS, "Ruby Leggings")
+			.item(ItemRegistry.RUBY_BOOTS, "Ruby Boots")
 
-		// Tools
-		addLang("ruby_pickaxe", lang().item(ItemRegistry.RUBY_PICKAXE, "Ruby Pickaxe"))
-		addLang("ruby_axe", lang().item(ItemRegistry.RUBY_AXE, "Ruby Axe"))
-		addLang("ruby_sword", lang().item(ItemRegistry.RUBY_SWORD, "Ruby Sword"))
-		addLang("ruby_shovel", lang().item(ItemRegistry.RUBY_SHOVEL, "Ruby Shovel"))
-		addLang("ruby_hoe", lang().item(ItemRegistry.RUBY_HOE, "Ruby Hoe"))
+			// Tools
+			.item(ItemRegistry.RUBY_PICKAXE, "Ruby Pickaxe")
+			.item(ItemRegistry.RUBY_AXE, "Ruby Axe")
+			.item(ItemRegistry.RUBY_SWORD, "Ruby Sword")
+			.item(ItemRegistry.RUBY_SHOVEL, "Ruby Shovel")
+			.item(ItemRegistry.RUBY_HOE, "Ruby Hoe")
 
-		// Blocks
-		addLang("ruby_block", lang().block(BlockRegistry.RUBY_BLOCK, "Ruby Block"))
-		addLang("ruby_ore", lang().block(BlockRegistry.RUBY_ORE, "Ruby Ore"))
-		addLang("ruby_wool", lang().block(BlockRegistry.RUBY_WOOL, "Ruby Wool"))
-		addLang("ruby_carpet", lang().block(BlockRegistry.RUBY_CARPET, "Ruby Carpet"))
-		addLang("logic_gate", lang().block(BlockRegistry.LOGIC_GATE, "Logic Gate"))
+			// Blocks
+			.block(BlockRegistry.RUBY_BLOCK, "Ruby Block")
+			.block(BlockRegistry.RUBY_ORE, "Ruby Ore")
+			.block(BlockRegistry.RUBY_WOOL, "Ruby Wool")
+			.block(BlockRegistry.RUBY_CARPET, "Ruby Carpet")
+			.block(BlockRegistry.LOGIC_GATE, "Logic Gate")
 
-		// Enchantments
-		addLang("stinger", lang().enchantment(EnchantmentRegistry.STINGER, "Stinger"))
+			// Enchantments
+			.enchantment(EnchantmentRegistry.STINGER, "Stinger")
 
-		// Entities
-		addLang("ruby_sheep", lang().entity(EntityTypeRegistry.RUBY_SHEEP, "Ruby Sheep"))
+			// Entities
+			.entity(EntityTypeRegistry.RUBY_SHEEP, "Ruby Sheep")
 
-		// Item groups (creative tabs)
-		addLang("ruby_tab", lang().itemGroup(id("ruby_tab"), "RubyMod"))
+			// Item groups (creative tabs)
+			.itemGroup(id("ruby_tab"), "RubyMod")
 
-		// Potions
-		addLang("motivation", lang().potion(id("motivation"), "Motivation"))
-		addLang("laziness", lang().potion(id("laziness"), "Laziness"))
+			// Potions
+			.potion(id("motivation"), "Motivation")
+			.potion(id("laziness"), "Laziness"))
 	}
 
-	private fun addLang(id: String, lang: JLang) {
-		RESOURCE_PACK.addLang(id(id), lang)
+	private fun addLang(lang: JLang) {
+		RESOURCE_PACK.addLang(id(RubyMod.MOD_ID), lang)
 	}
 
 	private fun addLangCustomNamespace(id: Identifier, lang: JLang) {
